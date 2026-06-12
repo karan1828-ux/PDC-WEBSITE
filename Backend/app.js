f298cfcdef45ef4e199d0a82e712bcfec966e0be
PrakharAgarwal17 <prakharagarwal17jul2006@gmail.com>
2026-06-12 18:36:08 +0530

    Done changes

--- Backend/app.js
+++ Backend/app.js
@@ -18,6 +18,8 @@ app.use(express.json())
 app.use(express.urlencoded({extended:true}))
 app.use(cookieParser())
 
-app.listen(3000,()=>{
-    console.log("Server is running on port 3000")
-})
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log(`Server running on ${PORT}`);
+});
--- Backend/config/PostgresConfig.js
+++ Backend/config/PostgresConfig.js
@@ -1,18 +1,25 @@
 import "dotenv/config";
 import pg from "pg";
 
-const {Pool}=pg
+const { Pool } = pg;
 
-const pool=new Pool({
-      user:process.env.PG_USERNAME,
-      host:process.env.PG_HOST,
-      database:process.env.PG_DATABASE,
-      password:process.env.PG_PASSWORD,
-      port:process.env.PG_PORT
-})
+const pool = new Pool({
+    user: process.env.PG_USERNAME,
+    host: process.env.PG_HOST,
+    database: process.env.PG_DATABASE,
+    password: process.env.PG_PASSWORD,
+    port: process.env.PG_PORT
+});
 
-pool.query("SELECT NOW()")
-  .then(() => console.log("Postgres connected"))
-  .catch(err => console.log(err));
+try {
+    await pool.connect();
+    console.log("Postgres connected");
+} catch (err) {
+    console.error("Database connection failed:", err);
+}
 
-export default pool
+pool.on("error", (err) => {
+    console.error("Unexpected PG Error:", err);
+});
+
+export default pool;
