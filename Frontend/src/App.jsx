fe21806c870a32c4a0c8e39561389ec033b1fc6f
Karan Gulati <karan.gulati1828@gmail.com>
2026-07-04 16:31:04 +0530

    fix: fixed the stashed error of other branch

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,30 +1,11 @@
-<<<<<<< Updated upstream
-import { useState, useEffect } from 'react'
-=======
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Teams from "./pages/Teams";
->>>>>>> Stashed changes
 import Home from './pages/Home'
 import Leaderboard from './pages/Leaderboard'
 import './App.css'
 
 function App() {
-  const [currentHash, setCurrentHash] = useState(window.location.hash);
-
-  useEffect(() => {
-    const handleHashChange = () => {
-      setCurrentHash(window.location.hash);
-    };
-    window.addEventListener('hashchange', handleHashChange);
-    return () => window.removeEventListener('hashchange', handleHashChange);
-  }, []);
-
   return (
-<<<<<<< Updated upstream
-    <div className="app">
-      {currentHash === '#personality' ? <Leaderboard /> : <Home />}
-    </div>
-=======
     <Router>
       <div className="min-h-screen flex flex-col">
         <Routes>
@@ -34,7 +15,6 @@ function App() {
         </Routes>
       </div>
     </Router>
->>>>>>> Stashed changes
   )
 }
 
