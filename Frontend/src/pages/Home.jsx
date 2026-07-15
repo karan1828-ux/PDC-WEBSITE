e1fb34383137ca05c7d46b5a73d674c52b18e842
TanCodeX <tanmaypatwary@gmail.com>
2026-07-15 13:23:09 +0530

    refactor: replace anchor tags with Link components and implement hash-based smooth scrolling in Home page

--- Frontend/src/pages/Home.jsx
+++ Frontend/src/pages/Home.jsx
@@ -1,3 +1,5 @@
+import { useEffect } from 'react'
+import { useLocation } from 'react-router-dom'
 import Navbar from '../components/Navbar/Navbar'
 import AnnouncementBar from '../components/AnnouncementBar/AnnouncementBar'
 import Hero from '../components/Hero/Hero'
@@ -8,6 +10,22 @@ import ContactUs from '../components/ContactUs/ContactUs'
 import Footer from '../components/Footer/Footer'
 
 function Home() {
+  const location = useLocation();
+
+  useEffect(() => {
+    if (location.hash) {
+      const id = location.hash.replace('#', '');
+      const element = document.getElementById(id);
+      if (element) {
+        setTimeout(() => {
+          element.scrollIntoView({ behavior: 'smooth' });
+        }, 100);
+      }
+    } else {
+      window.scrollTo({ top: 0, left: 0, behavior: 'smooth' });
+    }
+  }, [location]);
+
   return (
     <main className="flex flex-col min-h-screen">
       <header className="sticky top-0 z-50 w-full">
